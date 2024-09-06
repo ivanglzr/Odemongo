c349b92b2d7f2415e800af39c16a7b3d7fc9bbf9
Ivan <ivanglzr01@gmail.com>
2024-09-06 21:20:42 +0200

    export all from index

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-export * from "./lib/connect.js";
+import Database from "./lib/database.js";
+import Model from "./lib/model.js";
+
+import { ObjectIdSchema } from "./lib/schemas/";
+
+const schemas = {
+  ObjectIdSchema,
+};
+
+export { Database, Model, schemas };
