dec427ad7aa1f888c8352bc5fc0e385b035d2327
Ivan <ivanglzr01@gmail.com>
2024-09-14 21:21:22 +0200

    update exportations

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,7 @@
-export * from "./index.js";
+export { default as Database } from "./lib/database.js";
+
+export { default as Model } from "./lib/model.js";
+
+export * from "./lib/schemas/index.js";
+
 export as namespace Odemongo;
--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-import Database from "./lib/database.js";
-
-export { Database };
-export const connect = Database.connect;
+export { default as Database } from "./lib/database.js";
 
 export { default as Model } from "./lib/model.js";
 
-import { ObjectIdSchema } from "./lib/schemas/index.js";
-
-export const schemas = {
-  ObjectIdSchema,
-};
+export * from "./lib/schemas/index.js";
