626e49fb96561c2d177f4b51a589bf58dc62f760
Ivan <ivanglzr01@gmail.com>
2024-10-18 16:52:48 +0200

    docs: add documentation to the functions type

--- lib/types/functions.d.ts
+++ lib/types/functions.d.ts
@@ -1,3 +1,8 @@
+/**
+ * Use this function to get the mongoDB url and the database name
+ * @param {string} url The database url
+ * @returns {{mongoURL: string, databaseName: string}} The url and the db name
+ */
 export function parseMongoURL(url: string): {
   mongoURL: string;
   databaseName: string;
